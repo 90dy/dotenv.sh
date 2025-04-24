dce854e9197a3f97fad6c858090b54e6d5a36480
90DY <forward@90dy.ltd>
2025-04-24 14:35:38 +0200

    feat: add krew
    Signed-off-by: 90DY <forward@90dy.ltd>

--- android-studio.env.sh
+++ android-studio.env.sh
@@ -1,3 +1,5 @@
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_HOME=/Users/$USER/Library/Android/sdk
+export ANDROID_AVD_HOME=/Users/$USER/.android/avd
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
 
